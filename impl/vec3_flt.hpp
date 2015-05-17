ef1295f60630dbcb7455b9590f8fc1501e31cbe7
tufak <tufak064@gmail.com>
2015-05-17 20:41:48 +0200

    Created a specialization of basic_vec3 for simd::flt

---
+++ impl/vec3_flt.hpp
@@ -0,0 +1,83 @@
+#ifndef SIMDIFY_VEC3_FLT
+#define SIMDIFY_VEC3_FLT
+
+#include "../vec3.hpp"
+#include "../util/inline.hpp"
+
+#ifdef SIMDIFY_SSE
+
+#define INL SIMDIFY_FORCE_INLINE
+
+namespace simd {
+
+    template <>
+    struct basic_vec3<flt> {
+        using mm_t = flt::mm_t;
+        using fp_t = flt::fp_t;
+        using bitmask_t = flt::bitmask_t;
+        
+        union {
+            struct { flt x, y, z, extra; };
+            struct { sse mm; };
+        };
+
+        INL basic_vec3() = default;
+        INL basic_vec3(const basic_vec3& other) = default;
+        INL basic_vec3& operator=(const basic_vec3& other) = default;
+
+        INL explicit basic_vec3(const flt& t) : x(t), y(t), z(t) {}
+        INL explicit basic_vec3(fp_t t) : x(t), y(t), z(t) {}
+        INL explicit basic_vec3(const sse& w) : mm(w) {}
+
+        INL basic_vec3(const flt& tx, const flt& ty, const flt& tz) :
+            x(tx), y(ty), z(tz) {}
+        INL basic_vec3(fp_t tx, fp_t ty, fp_t tz) :
+            x(tx), y(ty), z(tz) {}
+
+        template <typename S>
+        INL explicit basic_vec3(const S& other) : x(other.x), y(other.y), z(other.z) {}
+
+        template <typename S>
+        INL basic_vec3& operator=(const S& other) {
+            x = flt(other.x); y = flt(other.y); z = flt(other.z);
+            return *this;
+        }
+
+        INL basic_vec3& operator[](std::size_t) { return *this; }
+        INL const basic_vec3& operator[](std::size_t) const { return *this; }
+    };
+
+    using vec3f = basic_vec3<flt>;
+
+    template <>
+    struct bitwise_not<vec3f> {
+        vec3f neg;
+        INL explicit bitwise_not(const vec3f& r) : neg(r) {}
+        INL operator const vec3f() const { return vec3f(sse(~neg.mm)); }
+    };
+
+    INL const vec3f operator&(const vec3f& l, const vec3f& r) { return vec3f(l.mm & r.mm); }
+    INL const vec3f operator|(const vec3f& l, const vec3f& r) { return vec3f(l.mm | r.mm); }
+    INL const vec3f operator^(const vec3f& l, const vec3f& r) { return vec3f(l.mm ^ r.mm); }
+    INL const bitwise_not<vec3f> operator~(const vec3f& l) { return bitwise_not<vec3f>(l); }
+    INL const vec3f operator<(const vec3f& l, const vec3f& r) { return vec3f(l.mm < r.mm); }
+    INL const vec3f operator>(const vec3f& l, const vec3f& r) { return vec3f(l.mm > r.mm); }
+    INL const vec3f operator<=(const vec3f& l, const vec3f& r) { return vec3f(l.mm <= r.mm); }
+    INL const vec3f operator>=(const vec3f& l, const vec3f& r) { return vec3f(l.mm >= r.mm); }
+    INL const vec3f operator==(const vec3f& l, const vec3f& r) { return vec3f(l.mm == r.mm); }
+    INL const vec3f operator!=(const vec3f& l, const vec3f& r) { return vec3f(l.mm != r.mm); }
+    INL const vec3f operator+(const vec3f& l, const vec3f& r) { return vec3f(l.mm + r.mm); }
+    INL const vec3f operator-(const vec3f& l, const vec3f& r) { return vec3f(l.mm - r.mm); }
+    INL const vec3f operator*(const vec3f& l, const vec3f& r) { return vec3f(l.mm * r.mm); }
+    INL const vec3f operator/(const vec3f& l, const vec3f& r) { return vec3f(l.mm / r.mm); }
+    INL const vec3f andnot(const vec3f& l, const vec3f& r) { return vec3f(andnot(l.mm, r.mm)); }
+    INL const vec3f min(const vec3f& l, const vec3f& r) { return vec3f(min(l.mm, r.mm)); }
+    INL const vec3f max(const vec3f& l, const vec3f& r) { return vec3f(max(l.mm, r.mm)); }
+    INL const vec3f cond(const vec3f& p, const vec3f& t, const vec3f& f) { return vec3f(cond(p.mm, t.mm, f.mm)); }
+}
+
+#undef INL
+
+#endif // SIMDIFY_SSE
+
+#endif // SIMDIFY_VEC3_FLT
--- vec3.hpp
+++ vec3.hpp
@@ -2,107 +2,17 @@
 #define SIMDIFY_VEC3
 
 #include "simdify.hpp"
-#include "util/inline.hpp"
-
-#define INL SIMDIFY_FORCE_INLINE
 
 namespace simd {
-
     template <typename T>
-    struct basic_vec3 {
-        using mm_t = typename T::mm_t;
-        using fp_t = typename T::fp_t;
-        using bitmask_t = typename T::bitmask_t;
-
-        T x, y, z;
-
-        INL basic_vec3() = default;
-        INL basic_vec3(const basic_vec3& other) = default;
-        INL basic_vec3& operator=(const basic_vec3& other) = default;
-
-        INL explicit basic_vec3(const T& t) : x(t), y(t), z(t) {}
-        
-        INL basic_vec3(const T& tx, const T& ty, const T& tz) :
-            x(tx), y(ty), z(tz) {}
-
-        template <typename S>
-        INL explicit basic_vec3(const S& other) : x(other.x), y(other.y), z(other.z) {}
-
-        template <typename S>
-        INL basic_vec3& operator=(const S& other) {
-            x = T(other.x); y = T(other.y); z = T(other.z);
-            return *this;
-        }
-
-        // the value received by subscript behaves as a triple of floats x, y, z
-        struct view {
-            fp_t& x; fp_t& y; fp_t& z;
-
-            INL view(basic_vec3& v, std::size_t i) : x(v.x.f[i]), y(v.y.f[i]), z(v.z.f[i]) {}
-
-            template <typename S>
-            INL view& operator=(const S& other) {
-                x = other.x; y = other.y; z = other.z;
-                return *this;
-            }
-
-            template <typename S>
-            INL explicit operator S() const {
-                return S(x, y, z);
-            }
-        };
-
-        INL view operator[](std::size_t i) { return view(*this, i); }
-        INL const view operator[](std::size_t i) const { return view(*this, i); }
-    };
-
-    template <typename T>
-    INL const basic_vec3<T> cross(const basic_vec3<T>& l, const basic_vec3<T>& r) {
-        return{ l.y*r.z - l.z*r.y, l.z*r.x - l.x*r.z, l.x*r.y - l.y*r.x };
-    }
-
-    template <typename T>
-    INL const T dot(const basic_vec3<T>& l, const basic_vec3<T>& r) {
-        return l.x*r.x + l.y*r.y + l.z*r.z;
-    }
-
-    // bitwise not with lazy evaluation
-    template <typename T>
-    struct bitwise_not<basic_vec3<T>> {
-        using tp = basic_vec3<T>;
-        tp neg;
-        INL explicit bitwise_not(const tp& r) : neg(r) {}
-        INL operator const tp() const { return{ ~neg.x, ~neg.y, ~neg.z }; }
-    };
-
-    template <typename T> INL const basic_vec3<T> operator&(const basic_vec3<T>& l, const basic_vec3<T>& r) { return{ l.x & r.x, l.y & r.y, l.z & r.z }; }
-    template <typename T> INL const basic_vec3<T> operator|(const basic_vec3<T>& l, const basic_vec3<T>& r) { return{ l.x | r.x, l.y | r.y, l.z | r.z }; }
-    template <typename T> INL const basic_vec3<T> operator^(const basic_vec3<T>& l, const basic_vec3<T>& r) { return{ l.x ^ r.x, l.y ^ r.y, l.z ^ r.z }; }
-    template <typename T> INL const bitwise_not<basic_vec3<T>> operator~(const basic_vec3<T>& l) { return bitwise_not<basic_vec3<T>>(l); }
-    template <typename T> INL const basic_vec3<T> operator<(const basic_vec3<T>& l, const basic_vec3<T>& r) { return{ l.x < r.x, l.y < r.y, l.z < r.z }; }
-    template <typename T> INL const basic_vec3<T> operator>(const basic_vec3<T>& l, const basic_vec3<T>& r) { return{ l.x > r.x, l.y > r.y, l.z > r.z }; }
-    template <typename T> INL const basic_vec3<T> operator<=(const basic_vec3<T>& l, const basic_vec3<T>& r) { return{ l.x <= r.x, l.y <= r.y, l.z <= r.z }; }
-    template <typename T> INL const basic_vec3<T> operator>=(const basic_vec3<T>& l, const basic_vec3<T>& r) { return{ l.x >= r.x, l.y >= r.y, l.z >= r.z }; }
-    template <typename T> INL const basic_vec3<T> operator==(const basic_vec3<T>& l, const basic_vec3<T>& r) { return{ l.x == r.x, l.y == r.y, l.z == r.z }; }
-    template <typename T> INL const basic_vec3<T> operator!=(const basic_vec3<T>& l, const basic_vec3<T>& r) { return{ l.x != r.x, l.y != r.y, l.z != r.z }; }
-    template <typename T> INL const basic_vec3<T> operator+(const basic_vec3<T>& l, const basic_vec3<T>& r) { return{ l.x + r.x, l.y + r.y, l.z + r.z }; }
-    template <typename T> INL const basic_vec3<T> operator-(const basic_vec3<T>& l, const basic_vec3<T>& r) { return{ l.x - r.x, l.y - r.y, l.z - r.z }; }
-    template <typename T> INL const basic_vec3<T> operator*(const basic_vec3<T>& l, const basic_vec3<T>& r) { return{ l.x * r.x, l.y * r.y, l.z * r.z }; }
-    template <typename T> INL const basic_vec3<T> operator/(const basic_vec3<T>& l, const basic_vec3<T>& r) { return{ l.x / r.x, l.y / r.y, l.z / r.z }; }
-    
-    template <typename T>
-    INL const basic_vec3<T> min(const basic_vec3<T>& l, const basic_vec3<T>& r) {
-        return{ min(l.x, r.x), min(l.y, r.y), min(l.z, r.z) };
-    }
-
-    template <typename T>
-    INL const basic_vec3<T> max(const basic_vec3<T>& l, const basic_vec3<T>& r) {
-        return{ max(l.x, r.x), max(l.y, r.y), max(l.z, r.z) };
-    }
-
-    using vec3 = basic_vec3<simd_t>;
+    struct basic_vec3;
 }
 
-#undef INL
+#include "impl/vec3_general.hpp"
+#include "impl/vec3_flt.hpp"
+
+namespace simd {
+    using vec3 = basic_vec3<simd_t>;
+}
 
 #endif // SIMDIFY_VEC3

c942bb81d1c2eba8842be0b7eeb524ab5273722d
tufak <tufak064@gmail.com>
2015-05-27 16:59:12 +0200

    Bad cast in aligned_free

--- util/malloc.hpp
+++ util/malloc.hpp
@@ -38,53 +38,9 @@ namespace simd {
     template <typename T>
     void aligned_free(T* aligned)
     {
-        auto orig = static_cast<void**>(aligned)[-1]; // restore orig from right before the start of user data
+        auto orig = reinterpret_cast<void**>(aligned)[-1]; // restore orig from right before the start of user data
         std::free(orig);
     }
-
-	/*// malloc-like aligned memory allocation, no constructors are called
-	template <typename T>
-	inline void aligned_malloc(T*& arr, std::size_t count = 1, std::size_t align = __alignof(T))
-	{
-		arr = (T*)_aligned_malloc(count * sizeof(T), align);
-	}
-
-	// free-like aligned memory deallocation, no destructors are called
-	template <typename T>
-	inline void aligned_free(T* arr)
-	{
-		_aligned_free(arr);
-	}
-
-	// new-like aligned memory allocation, constructors are called if necessary
-	template <typename T>
-	inline void aligned_new(T*& arr, std::size_t count = 1, std::size_t align = __alignof(T))
-	{
-		aligned_malloc(arr, count, align);
-
-		if (!detail::is_trivially_default_constructible<T>::value && arr != nullptr) {
-			auto begin = arr;
-			auto end = arr + count;
-			for (; begin != end; ++begin) {
-				new (begin)T;
-			}
-		}
-	}
-
-	// delete-like aligned memory deallocation, destructors are called if necessary
-	template <typename T>
-	inline void aligned_delete(T* arr, std::size_t count)
-	{
-		if (!std::is_trivially_destructible<T>::value && arr != nullptr) {
-			auto begin = arr;
-			auto end = arr + count;
-			for (; begin != end; ++begin) {
-				begin->~T();
-			}
-		}
-
-		aligned_free(arr);
-	}*/
 
     // aligned allocator
     template <typename T, std::size_t Align>

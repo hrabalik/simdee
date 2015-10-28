fd22dff123f9d37de243bf833137e5b4c9f4628c
Aleš Hrabalík <tufak064@gmail.com>
2015-10-28 22:48:26 +0100

    Cosmetic unit test changes

--- test/avx.cpp
+++ test/avx.cpp
@@ -9,15 +9,15 @@
 using T = simd::avx;
 using buf_t = std::array<T::fp_t, T::W>;
 
-alignas(T) const buf_t BA = {
+alignas(T) const buf_t bufA = {
     -0.27787193f, +0.70154146f, -2.05181630f, -0.35385000f,
     -0.82358653f, -1.57705702f, +0.50797465f, +0.28198406f,
 };
-alignas(T) const buf_t BB = {
+alignas(T) const buf_t bufB = {
     -0.23645458f, +2.02369089f, -2.25835397f, +2.22944568f,
     +0.33756370f, +1.00006082f, -1.66416447f, -0.59003456f,
 };
-alignas(T) const buf_t BC = {
+alignas(T) const buf_t bufC = {
     +0.42862268f, -1.03598478f, +1.87786546f, +0.94070440f,
     +0.78734578f, -0.87587426f, +0.31994913f, -0.55829428f,
 };
@@ -41,29 +41,25 @@ TEST_CASE("AVX", "[simd][x86][avx]") {
             T t;
         }
         SECTION("from mm") {
-            T::mm_t mm = _mm256_load_ps(BA.data());
+            T::mm_t mm = _mm256_load_ps(bufA.data());
             T t(mm);
-            REQUIRE(t.f == BA);
+            REQUIRE(t.f == bufA);
         }
-        //SECTION("from pointer") {
-        //    T t(BA.data());
-        //    REQUIRE(t.f == BA);
+        //SECTION("from aligned pointer") {
+        //    T t(bufA.data());
+        //    REQUIRE(t.f == bufA);
         //}
-        SECTION("from zero_t") {
-            T t(simd::ZERO);
-            for (auto val : t.i) REQUIRE(val == 0x00000000);
-        }
-        SECTION("from all_bits_t") {
-            T t(simd::ALL_BITS);
-            for (auto val : t.i) REQUIRE(val == 0xffffffff);
-        }
-        SECTION("from abs_mask_t") {
-            T t(simd::ABS_MASK);
-            for (auto val : t.i) REQUIRE(val == 0x7fffffff);
-        }
-        SECTION("from sign_bit_t") {
-            T t(simd::SIGN_BIT);
-            for (auto val : t.i) REQUIRE(val == 0x80000000);
+        SECTION("from special values") {
+            T t1(simd::ZERO);
+            T t2(simd::ALL_BITS);
+            T t3(simd::ABS_MASK);
+            T t4(simd::SIGN_BIT);
+            T t5(T::mask_t(0xdeadbeef));
+            for (auto val : t1.i) REQUIRE(val == 0x00000000);
+            for (auto val : t2.i) REQUIRE(val == 0xffffffff);
+            for (auto val : t3.i) REQUIRE(val == 0x7fffffff);
+            for (auto val : t4.i) REQUIRE(val == 0x80000000);
+            for (auto val : t5.i) REQUIRE(val == 0xdeadbeef);
         }
     }
     SECTION("operations") {

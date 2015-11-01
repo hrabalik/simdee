bcb5d5f2ef5652d2ec71a6afbccf218b50a6e73a
tufak <tufak064@gmail.com>
2015-11-01 01:26:42 +0100

    Added tou, tos, utof, stof

--- include/simdify/impl/expr.hpp
+++ include/simdify/impl/expr.hpp
@@ -81,6 +81,68 @@ namespace simd {
             // data
             T&& ref;
         };
+
+        template <typename T>
+        struct utof {
+            SIMDIFY_FORCE_INLINE explicit utof(T&& r) : ref(std::forward<T>(r)) {}
+
+            template <typename F_t>
+            F_t to() {
+                static_assert(std::is_floating_point<F_t>::value, "utof::to used to convert to a non-floating point type");
+                using u_t = simd::select_uint_t<sizeof(F_t)>;
+                return reinterpret_cast<const F_t&>(static_cast<u_t>(ref));
+            }
+
+            // data
+            T&& ref;
+        };
+
+        template <typename T>
+        struct stof {
+            SIMDIFY_FORCE_INLINE explicit stof(T&& r) : ref(std::forward<T>(r)) {}
+
+            template <typename F_t>
+            F_t to() {
+                static_assert(std::is_floating_point<F_t>::value, "stof::to() used to convert to a non-floating point type");
+                using s_t = simd::select_sint_t<sizeof(F_t)>;
+                return reinterpret_cast<const F_t&>(static_cast<s_t>(ref));
+            }
+
+            // data
+            T&& ref;
+        };
+
+        template <typename T>
+        struct tou {
+            using f_t = typename std::decay<T>::type;
+            using u_t = simd::select_uint_t<sizeof(f_t)>;
+            static_assert(std::is_floating_point<f_t>::value, "tou() must be used to convert a floating-point value");
+
+            SIMDIFY_FORCE_INLINE explicit tou(T&& r) : ref(std::forward<T>(r)) {}
+
+            SIMDIFY_FORCE_INLINE operator u_t() {
+                return reinterpret_cast<const u_t&>(ref);
+            }
+
+            // data
+            T&& ref;
+        };
+
+        template <typename T>
+        struct tos {
+            using f_t = typename std::decay<T>::type;
+            using s_t = simd::select_sint_t<sizeof(f_t)>;
+            static_assert(std::is_floating_point<f_t>::value, "tos() must be used to convert a floating-point value");
+
+            SIMDIFY_FORCE_INLINE explicit tos(T&& r) : ref(std::forward<T>(r)) {}
+
+            SIMDIFY_FORCE_INLINE operator s_t() {
+                return reinterpret_cast<const s_t&>(ref);
+            }
+
+            // data
+            T&& ref;
+        };
     }
 
     template <typename T>
@@ -99,6 +161,14 @@ namespace simd {
     SIMDIFY_FORCE_INLINE float sintf(T&& r) { return float(expr::sint<T&&>(std::forward<T>(r))); }
     template <typename T>
     SIMDIFY_FORCE_INLINE double sintd(T&& r) { return double(expr::sint<T&&>(std::forward<T>(r))); }
+    template <typename T>
+    SIMDIFY_FORCE_INLINE expr::utof<T&&> utof(T&& r) { return expr::utof<T&&>(std::forward<T>(r)); }
+    template <typename T>
+    SIMDIFY_FORCE_INLINE expr::stof<T&&> stof(T&& r) { return expr::stof<T&&>(std::forward<T>(r)); }
+    template <typename T>
+    SIMDIFY_FORCE_INLINE expr::tou<T&&> tou(T&& r) { return expr::tou<T&&>(std::forward<T>(r)); }
+    template <typename T>
+    SIMDIFY_FORCE_INLINE expr::tos<T&&> tos(T&& r) { return expr::tos<T&&>(std::forward<T>(r)); }
 }
 
 #endif // SIMDIFY_EXPR
--- test/expr.cpp
+++ test/expr.cpp
@@ -70,3 +70,73 @@ TEST_CASE("expr::uint, expr::sint", "[expr]") {
         //REQUIRE(d2 == simd::sintd(+9223372036854775807LL)); // fails, NaN != NaN
     }
 }
+
+TEST_CASE("expr::tou", "[expr]") {
+    SECTION("rvalues") {
+        REQUIRE(simd::tou(2.0f) == 0x40000000U);
+        REQUIRE(simd::tou(2.0) == 0x4000000000000000ULL);
+        REQUIRE(simd::tou(-6.2598534e18f) == 0xdeadbeefU);
+        REQUIRE(simd::tou(-1.1885959257070704e+148) == 0xdeadbeefdeadbeefULL);
+    }
+    SECTION("lvalues") {
+        float f = 2;
+        double d = 2;
+        REQUIRE(simd::tou(f) == 0x40000000U);
+        REQUIRE(simd::tou(d) == 0x4000000000000000ULL);
+    }
+}
+
+TEST_CASE("expr::tos", "[expr]") {
+    SECTION("rvalues") {
+        REQUIRE(simd::tos(-2.0f) == -0x40000000);
+        REQUIRE(simd::tos(-2.0) == -0x4000000000000000LL);
+    }
+    SECTION("lvalues") {
+        float f = -2;
+        double d = -2;
+        REQUIRE(simd::tos(f) == -0x40000000);
+        REQUIRE(simd::tos(d) == -0x4000000000000000LL);
+    }
+}
+
+TEST_CASE("expr::utof", "[expr]") {
+    SECTION("float") {
+        float f = 0;
+        f = simd::utof(0xdeadbeefU).to<float>();
+        REQUIRE(simd::tou(f) == 0xdeadbeefU);
+        f = simd::utof(std::numeric_limits<uint32_t>::min()).to<float>();
+        REQUIRE(simd::tou(f) == std::numeric_limits<uint32_t>::min());
+        f = simd::utof(std::numeric_limits<uint32_t>::max()).to<float>();
+        REQUIRE(simd::tou(f) == std::numeric_limits<uint32_t>::max());
+    }
+    SECTION("double") {
+        double d = 0;
+        d = simd::utof(0xdeadbeefdeadbeefULL).to<double>();
+        REQUIRE(simd::tou(d) == 0xdeadbeefdeadbeefULL);
+        d = simd::utof(std::numeric_limits<uint64_t>::min()).to<double>();
+        REQUIRE(simd::tou(d) == std::numeric_limits<uint64_t>::min());
+        d = simd::utof(std::numeric_limits<uint64_t>::max()).to<double>();
+        REQUIRE(simd::tou(d) == std::numeric_limits<uint64_t>::max());
+    }
+}
+
+TEST_CASE("expr::stof", "[expr]") {
+    SECTION("float") {
+        float f = 0;
+        f = simd::stof(-123456789).to<float>();
+        REQUIRE(simd::tos(f) == -123456789);
+        f = simd::stof(std::numeric_limits<int32_t>::min()).to<float>();
+        REQUIRE(simd::tos(f) == std::numeric_limits<int32_t>::min());
+        f = simd::stof(std::numeric_limits<int32_t>::max()).to<float>();
+        REQUIRE(simd::tos(f) == std::numeric_limits<int32_t>::max());
+    }
+    SECTION("double") {
+        double d = 0;
+        d = simd::stof(-1234567890123LL).to<double>();
+        REQUIRE(simd::tos(d) == -1234567890123LL);
+        d = simd::stof(std::numeric_limits<int64_t>::min()).to<double>();
+        REQUIRE(simd::tos(d) == std::numeric_limits<int64_t>::min());
+        d = simd::stof(std::numeric_limits<int64_t>::max()).to<double>();
+        REQUIRE(simd::tos(d) == std::numeric_limits<int64_t>::max());
+    }
+}

7e16ccc4f74d01bdda47bb2a5fd43bb4468946af
Aleš Hrabalík <tufak064@gmail.com>
2015-11-03 15:42:59 +0100

    Added constexpr to all structures and functions in expr.hpp

--- include/simdify/impl/expr.hpp
+++ include/simdify/impl/expr.hpp
@@ -8,7 +8,7 @@ namespace simd {
     namespace expr {
         template <typename T>
         struct aligned {
-            SIMDIFY_FORCE_INLINE explicit aligned(T* const& r) : ptr(r) {}
+            SIMDIFY_FORCE_INLINE constexpr explicit aligned(T* const& r) : ptr(r) {}
 
             template <typename Simd_t>
             SIMDIFY_FORCE_INLINE void operator=(const Simd_t& r) {
@@ -18,7 +18,7 @@ namespace simd {
             }
 
             template <typename F_t>
-            SIMDIFY_FORCE_INLINE const F_t* get_load() const {
+            SIMDIFY_FORCE_INLINE constexpr const F_t* get_load() const {
                 return reinterpret_cast<const F_t*>(ptr);
             }
 
@@ -28,7 +28,7 @@ namespace simd {
 
         template <typename T>
         struct unaligned {
-            SIMDIFY_FORCE_INLINE explicit unaligned(T* const& r) : ptr(r) {}
+            SIMDIFY_FORCE_INLINE constexpr explicit unaligned(T* const& r) : ptr(r) {}
 
             template <typename Simd_t>
             SIMDIFY_FORCE_INLINE void operator=(const Simd_t& r) {
@@ -38,7 +38,7 @@ namespace simd {
             }
 
             template <typename F_t>
-            SIMDIFY_FORCE_INLINE const F_t* get_load() const {
+            SIMDIFY_FORCE_INLINE constexpr const F_t* get_load() const {
                 return reinterpret_cast<const F_t*>(ptr);
             }
 
@@ -48,20 +48,20 @@ namespace simd {
 
         template <typename Crtp>
         struct tof {
-            SIMDIFY_FORCE_INLINE const Crtp& self() const { return static_cast<const Crtp&>(*this); }
+            SIMDIFY_FORCE_INLINE constexpr const Crtp& self() const { return static_cast<const Crtp&>(*this); }
 
             template <typename F_t>
-            SIMDIFY_FORCE_INLINE F_t to() const {
+            SIMDIFY_FORCE_INLINE constexpr F_t to() const {
                 return self().to<F_t>();
             }
         };
 
         template <typename T>
         struct utof : tof<utof<T>> {
-            SIMDIFY_FORCE_INLINE explicit utof(T&& r) : ref(std::forward<T>(r)) {}
+            SIMDIFY_FORCE_INLINE constexpr explicit utof(T&& r) : ref(std::forward<T>(r)) {}
 
             template <typename F_t>
-            SIMDIFY_FORCE_INLINE F_t to() const {
+            SIMDIFY_FORCE_INLINE constexpr F_t to() const {
                 static_assert(std::is_floating_point<F_t>::value, "utof() must only be used to produce floating-point types");
                 using u_t = simd::select_uint_t<sizeof(F_t)>;
                 return reinterpret_cast<const F_t&>(static_cast<const u_t&>(ref));
@@ -73,10 +73,10 @@ namespace simd {
 
         template <typename T>
         struct stof : tof<stof<T>> {
-            SIMDIFY_FORCE_INLINE explicit stof(T&& r) : ref(std::forward<T>(r)) {}
+            SIMDIFY_FORCE_INLINE constexpr explicit stof(T&& r) : ref(std::forward<T>(r)) {}
 
             template <typename F_t>
-            SIMDIFY_FORCE_INLINE F_t to() const {
+            SIMDIFY_FORCE_INLINE constexpr F_t to() const {
                 static_assert(std::is_floating_point<F_t>::value, "stof() must only be used to produce floating-point types");
                 using s_t = simd::select_sint_t<sizeof(F_t)>;
                 return reinterpret_cast<const F_t&>(static_cast<const s_t&>(ref));
@@ -88,7 +88,7 @@ namespace simd {
 
         struct zero : tof<zero> {
             template <typename F_t>
-            SIMDIFY_FORCE_INLINE F_t to() const {
+            SIMDIFY_FORCE_INLINE constexpr F_t to() const {
                 static_assert(std::is_floating_point<F_t>::value, "zero() must only be used to produce floating-point types");
                 using u_t = simd::select_uint_t<sizeof(F_t)>;
                 return reinterpret_cast<const F_t&>(static_cast<const u_t&>(0));
@@ -97,7 +97,7 @@ namespace simd {
 
         struct all_bits : tof<all_bits> {
             template <typename F_t>
-            SIMDIFY_FORCE_INLINE F_t to() const {
+            SIMDIFY_FORCE_INLINE constexpr F_t to() const {
                 static_assert(std::is_floating_point<F_t>::value, "all_bits() must only be used to produce floating-point types");
                 using u_t = simd::select_uint_t<sizeof(F_t)>;
                 return reinterpret_cast<const F_t&>(static_cast<const u_t&>(~u_t(0)));
@@ -107,7 +107,7 @@ namespace simd {
 
         struct sign_bit : tof<sign_bit> {
             template <typename F_t>
-            SIMDIFY_FORCE_INLINE F_t to() const {
+            SIMDIFY_FORCE_INLINE constexpr F_t to() const {
                 static_assert(std::is_floating_point<F_t>::value, "sign_bit() must only be used to produce floating-point types");
                 using u_t = simd::select_uint_t<sizeof(F_t)>;
                 return reinterpret_cast<const F_t&>(static_cast<const u_t&>(~(~u_t(0) >> 1)));
@@ -116,7 +116,7 @@ namespace simd {
 
         struct abs_mask : tof<abs_mask> {
             template <typename F_t>
-            SIMDIFY_FORCE_INLINE F_t to() const {
+            SIMDIFY_FORCE_INLINE constexpr F_t to() const {
                 static_assert(std::is_floating_point<F_t>::value, "abs_mask() must only be used to produce floating-point types");
                 using u_t = simd::select_uint_t<sizeof(F_t)>;
                 return reinterpret_cast<const F_t&>(static_cast<const u_t&>(~u_t(0) >> 1));
@@ -129,9 +129,9 @@ namespace simd {
             using u_t = simd::select_uint_t<sizeof(f_t)>;
             static_assert(std::is_floating_point<f_t>::value, "tou() must be used to convert a floating-point value");
 
-            SIMDIFY_FORCE_INLINE explicit tou(T&& r) : ref(std::forward<T>(r)) {}
+            SIMDIFY_FORCE_INLINE constexpr explicit tou(T&& r) : ref(std::forward<T>(r)) {}
 
-            SIMDIFY_FORCE_INLINE operator u_t() {
+            SIMDIFY_FORCE_INLINE constexpr operator u_t() const {
                 return reinterpret_cast<const u_t&>(ref);
             }
 
@@ -145,9 +145,9 @@ namespace simd {
             using s_t = simd::select_sint_t<sizeof(f_t)>;
             static_assert(std::is_floating_point<f_t>::value, "tos() must be used to convert a floating-point value");
 
-            SIMDIFY_FORCE_INLINE explicit tos(T&& r) : ref(std::forward<T>(r)) {}
+            SIMDIFY_FORCE_INLINE constexpr explicit tos(T&& r) : ref(std::forward<T>(r)) {}
 
-            SIMDIFY_FORCE_INLINE operator s_t() {
+            SIMDIFY_FORCE_INLINE constexpr operator s_t() const {
                 return reinterpret_cast<const s_t&>(ref);
             }
 
@@ -157,22 +157,22 @@ namespace simd {
     }
 
     template <typename T>
-    SIMDIFY_FORCE_INLINE expr::aligned<T> aligned(T* const& r) { return expr::aligned<T>(r); }
+    SIMDIFY_FORCE_INLINE constexpr expr::aligned<T> aligned(T* const& r) { return expr::aligned<T>(r); }
     template <typename T>
-    SIMDIFY_FORCE_INLINE expr::unaligned<T> unaligned(T* const& r) { return expr::unaligned<T>(r); }
+    SIMDIFY_FORCE_INLINE constexpr expr::unaligned<T> unaligned(T* const& r) { return expr::unaligned<T>(r); }
     template <typename T>
-    SIMDIFY_FORCE_INLINE expr::utof<T&&> utof(T&& r) { return expr::utof<T&&>(std::forward<T>(r)); }
+    SIMDIFY_FORCE_INLINE constexpr expr::utof<T&&> utof(T&& r) { return expr::utof<T&&>(std::forward<T>(r)); }
     template <typename T>
-    SIMDIFY_FORCE_INLINE expr::stof<T&&> stof(T&& r) { return expr::stof<T&&>(std::forward<T>(r)); }
+    SIMDIFY_FORCE_INLINE constexpr expr::stof<T&&> stof(T&& r) { return expr::stof<T&&>(std::forward<T>(r)); }
     template <typename T>
-    SIMDIFY_FORCE_INLINE expr::tou<T&&> tou(T&& r) { return expr::tou<T&&>(std::forward<T>(r)); }
+    SIMDIFY_FORCE_INLINE constexpr expr::tou<T&&> tou(T&& r) { return expr::tou<T&&>(std::forward<T>(r)); }
     template <typename T>
-    SIMDIFY_FORCE_INLINE expr::tos<T&&> tos(T&& r) { return expr::tos<T&&>(std::forward<T>(r)); }
+    SIMDIFY_FORCE_INLINE constexpr expr::tos<T&&> tos(T&& r) { return expr::tos<T&&>(std::forward<T>(r)); }
 
-    SIMDIFY_FORCE_INLINE expr::zero zero() { return expr::zero{}; }
-    SIMDIFY_FORCE_INLINE expr::all_bits all_bits() { return expr::all_bits{}; }
-    SIMDIFY_FORCE_INLINE expr::sign_bit sign_bit() { return expr::sign_bit{}; }
-    SIMDIFY_FORCE_INLINE expr::abs_mask abs_mask() { return expr::abs_mask{}; }
+    SIMDIFY_FORCE_INLINE constexpr expr::zero zero() { return expr::zero{}; }
+    SIMDIFY_FORCE_INLINE constexpr expr::all_bits all_bits() { return expr::all_bits{}; }
+    SIMDIFY_FORCE_INLINE constexpr expr::sign_bit sign_bit() { return expr::sign_bit{}; }
+    SIMDIFY_FORCE_INLINE constexpr expr::abs_mask abs_mask() { return expr::abs_mask{}; }
 }
 
 #endif // SIMDIFY_EXPR

e0a8cc9e7fc0ea6a137b5bb59f9b6383374fd985
Aleš Hrabalík <tufak064@gmail.com>
2015-11-13 17:23:38 +0100

    Iterators for structure_of_arrays (work in progress)

--- include/simdify/impl/structure_of_arrays.hpp
+++ include/simdify/impl/structure_of_arrays.hpp
@@ -4,7 +4,11 @@
 #include "../simdify.hpp"
 #include "../containers.hpp"
 #include "../util/inline.hpp"
+#include "../util/malloc.hpp"
+#include "expr.hpp"
 #include "named_array.hpp"
+#include <array>
+#include <vector>
 
 namespace simd {
     template <typename Simd_t, id... Ids>
@@ -12,43 +16,70 @@ namespace simd {
 
     template <typename Mm_t, typename F_t, typename Simd_t, id...Ids>
     struct structure_of_arrays<simd_base<Mm_t, F_t, Simd_t>, Ids...> {
-        static constexpr std::size_t N = sizeof...(Ids);
         using simd_t = Simd_t;
         using f_t = F_t;
         using mm_t = Mm_t;
+        enum : std::size_t { N = sizeof...(Ids), W = simd_t::W };
 
-        template <typename T>
-        struct iterator_base : named_array<T, Ids...> {};
+        struct iterator {
+            using buffer_t = named_array<expr::aligned<f_t>, Ids...>;
 
-        struct iterator : iterator_base<simd_t> {};
+            template <typename Func>
+            struct TransformItem {
+                iterator& m_it;
+                Func m_f;
 
-        struct const_iterator : iterator_base<const simd_t> {};
+                SIMDIFY_FORCE_INLINE constexpr TransformItem(iterator& it, Func&& f) :
+                    m_it(it), m_f(std::move(f)) {}
 
-        template <typename T>
-        struct tail_iterator_base : named_array<T, Ids...> {};
+                template <std::size_t I>
+                SIMDIFY_FORCE_INLINE constexpr void perform() const {
+                    m_f(simd::get<I>(m_it.m_buf).ptr);
+                }
+            };
 
-        struct tail_iterator : tail_iterator_base<f_t&> {};
+            iterator(structure_of_arrays& from, std::size_t index) {
+                detail::ForEach(N)::perform(TransformItem(*this, [&from, index](f_t*& ptr) {
+                    ptr = std::get<N>(from.m_data).data() + from;
+                }));
+            }
 
-        struct const_tail_iterator : tail_iterator_base<const f_t&> {};
+            iterator& operator++() {
+                detail::ForEach(N)::perform(TransformItem(*this, [](f_t*& ptr) {
+                    ptr += W;
+                }));
+                return *this;
+            }
 
-        iterator begin();
-        iterator end();
-        const_iterator begin() const;
-        const_iterator end() const;
-        const_iterator cbegin() const;
-        const_iterator cend() const;
-        iterator bodybegin();
-        iterator bodyend();
-        const_iterator bodybegin() const;
-        const_iterator bodyend() const;
-        const_iterator bodycbegin() const;
-        const_iterator bodycend() const;
-        tail_iterator tailbegin();
-        tail_iterator tailend();
-        const_tail_iterator tailbegin() const;
-        const_tail_iterator tailend() const;
-        const_tail_iterator tailcbegin() const;
-        const_tail_iterator tailcend() const;
+            bool operator==(const iterator& rhs) { return simd::get<0>(m_buf).ptr == simd::get<0>(rhs.m_buf).ptr; }
+            bool operator!=(const iterator& rhs) { return simd::get<0>(m_buf).ptr != simd::get<0>(rhs.m_buf).ptr; }
+            buffer_t& operator*() const { return m_buf; }
+            buffer_t& operator->() const { return m_buf; }
+
+        private:
+            buffer_t m_buf;
+        };
+
+        //iterator begin();
+        //iterator end();
+        //const_iterator begin() const;
+        //const_iterator end() const;
+        //const_iterator cbegin() const;
+        //const_iterator cend() const;
+        //iterator bodybegin();
+        //iterator bodyend();
+        //const_iterator bodybegin() const;
+        //const_iterator bodyend() const;
+        //const_iterator bodycbegin() const;
+        //const_iterator bodycend() const;
+        //tail_iterator tailbegin();
+        //tail_iterator tailend();
+        //const_tail_iterator tailbegin() const;
+        //const_tail_iterator tailend() const;
+        //const_tail_iterator tailcbegin() const;
+        //const_tail_iterator tailcend() const;
+    private:
+        std::array<std::vector<f_t, aligned_allocator<f_t, alignof(mm_t)>>, N> m_data;
     };
 }
 

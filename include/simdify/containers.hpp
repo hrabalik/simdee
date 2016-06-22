cb18878a397805a0535236013a8845fe4a2a8b75
Aleš Hrabalík <tufak064@gmail.com>
2016-06-22 23:34:47 +0200

    Remove containers, reducing simdify to SIMD types and storage only

--- include/simdify/containers.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef SIMDIFY_CONTAINERS_HPP
-#define SIMDIFY_CONTAINERS_HPP
-
-#include "simd_types.hpp"
-#include "containers/structure_of_arrays.hpp"
-#include "containers/array_of_structures.hpp"
-#include "containers/array_of_vectors.hpp"
-
-#endif // SIMDIFY_CONTAINERS_HPP

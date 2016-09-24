286465c79aec3f36ba1284f1e7d37f91472ede79
Aleš Hrabalík <tufak064@gmail.com>
2016-09-24 18:02:51 +0300

    Create an alternate implementation of storage
    Specialize for SIMD width 1 to remove ambiguities when using init list to
    create storage.

--- include/simdify/common/storage_impl.hpp
+++ include/simdify/common/storage_impl.hpp
@@ -15,10 +15,10 @@ namespace simd {
     struct storage;
 
     //
-    // storage for SIMD types
+    // storage specialization for SIMD types with width > 1
     //
     template <typename Simd_t>
-    struct alignas(Simd_t)storage<Simd_t, typename std::enable_if<is_simd_type<Simd_t>::value>::type> : std::array<typename Simd_t::scalar_t, Simd_t::W> {
+    struct alignas(Simd_t)storage<Simd_t, typename std::enable_if<(Simd_t::W > 1)>::type> : std::array<typename Simd_t::scalar_t, Simd_t::W> {
         using stored_t = Simd_t;
         using scalar_t = typename Simd_t::scalar_t;
         using array_t = std::array<scalar_t, Simd_t::W>;
@@ -34,6 +34,30 @@ namespace simd {
             static_cast<array_t&>(*this) = rhs;
             return *this;
         }
+        SIMDIFY_INL storage& operator=(const Simd_t& rhs) {
+            rhs.aligned_store(data());
+            return *this;
+        }
+
+        // implicit conversion to Simd_t
+        SIMDIFY_INL operator Simd_t() const { Simd_t s; s.aligned_load(data()); return s; }
+    };
+
+    //
+    // storage specialization for SIMD types with width == 0
+    //
+    template <typename Simd_t>
+    struct storage<Simd_t, typename std::enable_if<(Simd_t::W == 1)>::type> : std::array<typename Simd_t::scalar_t, Simd_t::W> {
+        using stored_t = Simd_t;
+        using scalar_t = typename Simd_t::scalar_t;
+        using array_t = std::array<scalar_t, Simd_t::W>;
+        using array_t::data;
+
+        SIMDIFY_INL constexpr storage() = default;
+        SIMDIFY_INL constexpr storage(const storage&) = default;
+        SIMDIFY_INL storage(const Simd_t& rhs) { rhs.aligned_store(data()); }
+
+        SIMDIFY_INL storage& operator=(const storage&) = default;
         SIMDIFY_INL storage& operator=(const Simd_t& rhs) {
             rhs.aligned_store(data());
             return *this;

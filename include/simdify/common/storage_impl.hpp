aba7daf7db8e4b526a66bd3ef876267c9920b833
Aleš Hrabalík <tufak064@gmail.com>
2016-09-24 18:43:00 +0300

    Handle braced init for storage with init list

--- include/simdify/common/storage_impl.hpp
+++ include/simdify/common/storage_impl.hpp
@@ -5,59 +5,35 @@
 #include "../simd_types/common.hpp"
 #include "../util/inline.hpp"
 #include <array>
+#include <algorithm>
+#include <initializer_list>
 
 namespace simd {
 
     //
     // storage for SIMD types
     //
-    template <typename T, typename Enable = void>
-    struct storage;
-
-    //
-    // storage specialization for SIMD types with width > 1
-    //
     template <typename Simd_t>
-    struct alignas(Simd_t)storage<Simd_t, typename std::enable_if<(Simd_t::W > 1)>::type> : std::array<typename Simd_t::scalar_t, Simd_t::W> {
+    struct alignas(Simd_t)storage : std::array<typename Simd_t::scalar_t, Simd_t::W> {
         using stored_t = Simd_t;
         using scalar_t = typename Simd_t::scalar_t;
         using array_t = std::array<scalar_t, Simd_t::W>;
         using array_t::data;
+        using array_t::begin;
 
         SIMDIFY_INL constexpr storage() = default;
         SIMDIFY_INL constexpr storage(const storage&) = default;
-        SIMDIFY_INL constexpr storage(const array_t& rhs) : array_t(rhs) {}
         SIMDIFY_INL explicit storage(const Simd_t& rhs) { rhs.aligned_store(data()); }
+
+        storage(std::initializer_list<scalar_t> list) {
+            std::copy(list.begin(), list.end(), begin());
+        }
 
         SIMDIFY_INL storage& operator=(const storage&) = default;
         SIMDIFY_INL storage& operator=(const array_t& rhs) {
             static_cast<array_t&>(*this) = rhs;
             return *this;
         }
-        SIMDIFY_INL storage& operator=(const Simd_t& rhs) {
-            rhs.aligned_store(data());
-            return *this;
-        }
-
-        // implicit conversion to Simd_t
-        SIMDIFY_INL operator Simd_t() const { Simd_t s; s.aligned_load(data()); return s; }
-    };
-
-    //
-    // storage specialization for SIMD types with width == 0
-    //
-    template <typename Simd_t>
-    struct storage<Simd_t, typename std::enable_if<(Simd_t::W == 1)>::type> : std::array<typename Simd_t::scalar_t, Simd_t::W> {
-        using stored_t = Simd_t;
-        using scalar_t = typename Simd_t::scalar_t;
-        using array_t = std::array<scalar_t, Simd_t::W>;
-        using array_t::data;
-
-        SIMDIFY_INL constexpr storage() = default;
-        SIMDIFY_INL constexpr storage(const storage&) = default;
-        SIMDIFY_INL storage(const Simd_t& rhs) { rhs.aligned_store(data()); }
-
-        SIMDIFY_INL storage& operator=(const storage&) = default;
         SIMDIFY_INL storage& operator=(const Simd_t& rhs) {
             rhs.aligned_store(data());
             return *this;

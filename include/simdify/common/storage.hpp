318e7429e530922918117602dbd124bb01ddef5c
Aleš Hrabalík <tufak064@gmail.com>
2016-09-24 16:31:11 +0300

    Remove storage specialization for scalars
    Generalized storage not needed since containers have been removed.

--- include/simdify/common/storage.hpp
+++ include/simdify/common/storage.hpp
@@ -9,13 +9,13 @@
 namespace simd {
 
     //
-    // storage for SIMD or floating-point types
+    // storage for SIMD types
     //
     template <typename T, typename Enable = void>
     struct storage;
 
     //
-    // specialized storage for SIMD types
+    // storage for SIMD types
     //
     template <typename Simd_t>
     struct alignas(Simd_t)storage<Simd_t, typename std::enable_if<is_simd_type<Simd_t>::value>::type> : std::array<typename Simd_t::scalar_t, Simd_t::W> {
@@ -41,35 +41,6 @@ namespace simd {
 
         // implicit conversion to Simd_t
         SIMDIFY_INL operator Simd_t() const { Simd_t s; s.aligned_load(data()); return s; }
-    };
-
-    //
-    // specialized storage for arithmetic types
-    //
-    template <typename Scalar_t>
-    struct storage<Scalar_t, typename std::enable_if<std::is_arithmetic<Scalar_t>::value>::type> : std::array<Scalar_t, 1> {
-        using stored_t = Scalar_t;
-        using scalar_t = Scalar_t;
-        using array_t = std::array<Scalar_t, 1>;
-        using array_t::data;
-
-        SIMDIFY_INL constexpr storage() = default;
-        SIMDIFY_INL constexpr storage(const storage&) = default;
-        SIMDIFY_INL constexpr storage(const array_t& rhs) : array_t{ rhs } {}
-        SIMDIFY_INL explicit storage(const Scalar_t& rhs) { *data() = rhs; }
-
-        SIMDIFY_INL storage& operator=(const storage&) = default;
-        SIMDIFY_INL storage& operator=(const array_t& rhs) {
-            static_cast<array_t&>(*this) = rhs;
-            return *this;
-        }
-        SIMDIFY_INL storage& operator=(const Scalar_t& rhs) {
-            *data() = rhs;
-            return *this;
-        }
-
-        // implicit conversion to Scalar_t
-        SIMDIFY_INL operator Scalar_t() const { return *data(); }
     };
 
     template <typename T>

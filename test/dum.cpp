d575c743671847207b91333075eb194b3de96cdb
Aleš Hrabalík <tufak064@gmail.com>
2016-09-24 18:05:31 +0300

    Use storage instead of array_t in tests

--- test/dum.cpp
+++ test/dum.cpp
@@ -13,27 +13,27 @@ using F = simd::dumf;
 using U = simd::dumu;
 using S = simd::dums;
 
-alignas(F) const F::array_t bufAF = { {
-        -0.27787193f,
-    } };
-alignas(F) const F::array_t bufBF = { {
-        -0.23645458f,
-    } };
-alignas(U) const U::array_t bufAU = { {
-        1753029375U,
-    } };
-alignas(U) const U::array_t bufBU = { {
-        1679702461U,
-    } };
-alignas(S) const S::array_t bufAS = { {
-        -1712190449,
-    } };
-alignas(S) const S::array_t bufBS = { {
-        724135231,
-    } };
-alignas(F) const F::array_t bufZF = { { 0 } };
-alignas(U) const U::array_t bufZU = { { 0 } };
-alignas(S) const S::array_t bufZS = { { 0 } };
+const simd::storage<F> bufAF = { {
+    -0.27787193f,
+} };
+const simd::storage<F> bufBF = { {
+    -0.23645458f,
+} };
+const simd::storage<U> bufAU = { {
+    1753029375U,
+} };
+const simd::storage<U> bufBU = { {
+    1679702461U,
+} };
+const simd::storage<S> bufAS = { {
+    -1712190449,
+} };
+const simd::storage<S> bufBS = { {
+    724135231,
+} };
+const simd::storage<F> bufZF = { { 0 } };
+const simd::storage<U> bufZU = { { 0 } };
+const simd::storage<S> bufZS = { { 0 } };
 
 #define SIMD_TYPE "DUM"
 #define SIMD_TEST_TAG "[simd_types][dum]"
--- test/sse.cpp
+++ test/sse.cpp
@@ -13,27 +13,27 @@ using F = simd::ssef;
 using U = simd::sseu;
 using S = simd::sses;
 
-alignas(F) const F::array_t bufAF = { {
-        -0.27787193f, +0.70154146f, -2.05181630f, +2.22944568f,
-    } };
-alignas(F) const F::array_t bufBF = { {
-        -0.23645458f, +2.02369089f, -2.25835397f, +2.22944568f,
-    } };
-alignas(U) const U::array_t bufAU = { {
-        1753029375U, 1117080442U, 3817141237U, 3761735248U,
-    } };
-alignas(U) const U::array_t bufBU = { {
-        1679702461U, 2102346647U, 480083363U, 779803484U,
-    } };
-alignas(S) const S::array_t bufAS = { {
-        -1712190449, -48692967, -214510247, 440646957,
-    } };
-alignas(S) const S::array_t bufBS = { {
-        724135231, 56848532, 64122653, 1641874147,
-    } };
-alignas(F) const F::array_t bufZF = { { 0, 0, 0, 0 } };
-alignas(U) const U::array_t bufZU = { { 0, 0, 0, 0 } };
-alignas(S) const S::array_t bufZS = { { 0, 0, 0, 0 } };
+const simd::storage<F> bufAF = { {
+    -0.27787193f, +0.70154146f, -2.05181630f, +2.22944568f,
+} };
+const simd::storage<F> bufBF = { {
+    -0.23645458f, +2.02369089f, -2.25835397f, +2.22944568f,
+} };
+const simd::storage<U> bufAU = { {
+    1753029375U, 1117080442U, 3817141237U, 3761735248U,
+} };
+const simd::storage<U> bufBU = { {
+    1679702461U, 2102346647U, 480083363U, 779803484U,
+} };
+const simd::storage<S> bufAS = { {
+    -1712190449, -48692967, -214510247, 440646957,
+} };
+const simd::storage<S> bufBS = { {
+    724135231, 56848532, 64122653, 1641874147,
+} };
+const simd::storage<F> bufZF = { { 0, 0, 0, 0 } };
+const simd::storage<U> bufZU = { { 0, 0, 0, 0 } };
+const simd::storage<S> bufZS = { { 0, 0, 0, 0 } };
 
 #define SIMD_TYPE "SSE"
 #define SIMD_TEST_TAG "[simd_types][sse]"

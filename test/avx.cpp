6fedf2f3c220b6f634ef2d7a1ee46b79443628f4
Aleš Hrabalík <tufak064@gmail.com>
2016-09-24 18:53:43 +0300

    Use single brackets for initializing test buffers

--- test/avx.cpp
+++ test/avx.cpp
@@ -13,33 +13,33 @@ using F = simd::avxf;
 using U = simd::avxu;
 using S = simd::avxs;
 
-const simd::storage<F> bufAF = { {
+const simd::storage<F> bufAF {
     -0.27787193f, +0.70154146f, -2.05181630f, +2.22944568f,
     -0.82358653f, -1.57705702f, +0.50797465f, -0.59003456f,
-} };
-const simd::storage<F> bufBF = { {
+};
+const simd::storage<F> bufBF {
     -0.23645458f, +2.02369089f, -2.25835397f, +2.22944568f,
     +0.33756370f, -0.87587426f, -1.66416447f, -0.59003456f,
-} };
-const simd::storage<U> bufAU = { {
+};
+const simd::storage<U> bufAU {
     1753029375U, 1117080442U, 3817141237U, 3761735248U,
     244284091U, 1874242234U, 3400252368U, 2648404707U,
-} };
-const simd::storage<U> bufBU = { {
+};
+const simd::storage<U> bufBU {
     1679702461U, 2102346647U, 480083363U, 779803484U,
     1213208312U, 2121108407U, 2718691794U, 3724041010U,
-} };
-const simd::storage<S> bufAS = { {
+};
+const simd::storage<S> bufAS {
     -1712190449, -48692967, -214510247, 440646957,
     471289320, 614985478, -2035465541, 248883859,
-} };
-const simd::storage<S> bufBS = { {
+};
+const simd::storage<S> bufBS {
     724135231, 56848532, 64122653, 1641874147,
     -899302812, -2112882416, 77287484, 1066617619,
-} };
-const simd::storage<F> bufZF = { { 0, 0, 0, 0, 0, 0, 0, 0 } };
-const simd::storage<U> bufZU = { { 0, 0, 0, 0, 0, 0, 0, 0 } };
-const simd::storage<S> bufZS = { { 0, 0, 0, 0, 0, 0, 0, 0 } };
+};
+const simd::storage<F> bufZF { 0, 0, 0, 0, 0, 0, 0, 0 };
+const simd::storage<U> bufZU { 0, 0, 0, 0, 0, 0, 0, 0 };
+const simd::storage<S> bufZS { 0, 0, 0, 0, 0, 0, 0, 0 };
 
 #define SIMD_TYPE "AVX"
 #define SIMD_TEST_TAG "[simd_types][avx]"
--- test/dum.cpp
+++ test/dum.cpp
@@ -13,27 +13,27 @@ using F = simd::dumf;
 using U = simd::dumu;
 using S = simd::dums;
 
-const simd::storage<F> bufAF = { {
+const simd::storage<F> bufAF {
     -0.27787193f,
-} };
-const simd::storage<F> bufBF = { {
+};
+const simd::storage<F> bufBF {
     -0.23645458f,
-} };
-const simd::storage<U> bufAU = { {
+};
+const simd::storage<U> bufAU {
     1753029375U,
-} };
-const simd::storage<U> bufBU = { {
+};
+const simd::storage<U> bufBU {
     1679702461U,
-} };
-const simd::storage<S> bufAS = { {
+};
+const simd::storage<S> bufAS {
     -1712190449,
-} };
-const simd::storage<S> bufBS = { {
+};
+const simd::storage<S> bufBS {
     724135231,
-} };
-const simd::storage<F> bufZF = { { 0 } };
-const simd::storage<U> bufZU = { { 0 } };
-const simd::storage<S> bufZS = { { 0 } };
+};
+const simd::storage<F> bufZF { 0 };
+const simd::storage<U> bufZU { 0 };
+const simd::storage<S> bufZS { 0 };
 
 #define SIMD_TYPE "DUM"
 #define SIMD_TEST_TAG "[simd_types][dum]"

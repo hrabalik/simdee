420e3c5248ec769e7b210a4823e4fc4160df0983
Aleš Hrabalík <tufak064@gmail.com>
2016-09-25 19:16:08 +0300

    Add mask

---
+++ include/simdify/common/mask.hpp
@@ -0,0 +1,38 @@
+#ifndef SIMDIFY_COMMON_MASK_HPP
+#define SIMDIFY_COMMON_MASK_HPP
+
+#include "../util/inline.hpp"
+#include "../util/trivial_type.hpp"
+#include <cstdint>
+
+namespace simd {
+    namespace impl {
+        // result of the mask operation
+        template <uint32_t AllBitsMask>
+        struct mask {
+            SIMDIFY_TRIVIAL_TYPE(mask);
+
+            SIMDIFY_INL constexpr mask(uint32_t i) : value(i) {}
+
+            SIMDIFY_INL constexpr mask operator&(mask r) const { return mask(value & r.value); }
+            SIMDIFY_INL constexpr mask operator|(mask r) const { return mask(value | r.value); }
+            SIMDIFY_INL constexpr mask operator^(mask r) const { return mask(value ^ r.value); }
+            SIMDIFY_INL mask& operator&=(mask r) { value &= r.value; return *this; }
+            SIMDIFY_INL mask& operator|=(mask r) { value |= r.value; return *this; }
+            SIMDIFY_INL mask& operator^=(mask r) { value ^= r.value; return *this; }
+            SIMDIFY_INL constexpr mask operator~() const { return mask(value ^ AllBitsMask); }
+            SIMDIFY_INL constexpr bool any() const { return value != 0; }
+            SIMDIFY_INL constexpr bool all() const { return value == AllBitsMaske; }
+            SIMDIFY_INL constexpr bit_iterator begin() const { return bit_iterator(value); }
+            SIMDIFY_INL constexpr bit_iterator end() const { return bit_iterator(value); }
+
+            //data
+            uint32_t value;
+        };
+    }
+
+    template <typename T>
+    using mask = typename T::mask_t;
+}
+
+#endif // SIMDIFY_COMMON_MASK_HPP

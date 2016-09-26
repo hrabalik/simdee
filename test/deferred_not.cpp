134c1c0e73161abec0ed73883681aaf3249b3788
Aleš Hrabalík <tufak064@gmail.com>
2016-09-26 22:53:59 +0300

    Add tests for deferred_not

---
+++ test/deferred_not.cpp
@@ -0,0 +1,60 @@
+#define SIMDIFY_NEED_SSE 1
+#include <simdify/simdify.hpp>
+
+#include "catch.hpp"
+#include <type_traits>
+
+using U = simd::sseu;
+const U::storage_t dataA{ 1753029375U, 1117080442U, 3817141237U, 3761735248U };
+
+using not_U = simd::expr::deferred_not<U>;
+
+TEST_CASE("deferred_not types", "[deferred_not]") {
+    REQUIRE((std::is_same<U::vector_t, not_U::vector_t>::value));
+    REQUIRE((std::is_same<U::scalar_t, not_U::scalar_t>::value));
+    REQUIRE((std::is_same<U::mask_t, not_U::mask_t>::value));
+    REQUIRE((std::is_same<U::storage_t, not_U::storage_t>::value));
+    REQUIRE((std::is_same<U::binary_op_t, not_U::binary_op_t>::value));
+    REQUIRE((std::size_t(U::width) == not_U::width));
+}
+
+TEST_CASE("deferred_not arithmetic", "[deferred_not]") {
+    U ua = ~dataA;
+    not_U nua(dataA);
+    REQUIRE(all(ua == nua));
+}
+
+TEST_CASE("deferred_not methods", "[deferred_not]") {
+    const U ua = ~dataA;
+    const not_U nua(dataA);
+    SECTION("data") {
+        REQUIRE(all(U(ua.data()) == U(nua.data())));
+    }
+    SECTION("store") {
+        SECTION("aligned") {
+            U::storage_t res_ua{}, res_nua{};
+            ua.aligned_store(res_ua.data());
+            nua.aligned_store(res_nua.data());
+            REQUIRE((res_ua == res_nua));
+        }
+        SECTION("unaligned") {
+            U::storage_t res_ua{}, res_nua{};
+            ua.unaligned_store(res_ua.data());
+            nua.unaligned_store(res_nua.data());
+            REQUIRE((res_ua == res_nua));
+        }
+        SECTION("interleaved") {
+            U::storage_t res_ua[3]{}, res_nua[3]{};
+            ua.interleaved_store(res_ua[0].data(), 3);
+            nua.interleaved_store(res_nua[0].data(), 3);
+            REQUIRE((res_ua[0] == res_nua[0]));
+            REQUIRE((res_ua[1] == res_nua[1]));
+            REQUIRE((res_ua[2] == res_nua[2]));
+        }
+    }
+    SECTION("reduce") {
+        auto res_ua = ua.reduce(simd::operator^).first_element();
+        auto res_nua = nua.reduce(simd::operator^).first_element();
+        REQUIRE(res_ua == res_nua);
+    }
+}

9b9e2d64b27121d0a1575ea1f2b987ee4a59ce72
Aleš Hrabalík <tufak064@gmail.com>
2016-10-03 23:58:57 +0300

    Implement dual_base #8

--- include/simdee/simd_types/dual.hpp
+++ include/simdee/simd_types/dual.hpp
@@ -33,28 +33,80 @@ namespace sd {
         using storage_t = impl::storage<simd_t, scalar_t, alignof(vector_t)>;
     };
 
+    template <typename Crtp>
+    struct dual_base : simd_base<Crtp> {
+    protected:
+        using T = typename simd_base<Crtp>::vector_t::first_type;
+        using simd_base<Crtp>::mm;
+
+    public:
+        using vector_t = typename simd_base<Crtp>::vector_t;
+        using scalar_t = typename simd_base<Crtp>::scalar_t;
+        using element_t = typename simd_base<Crtp>::element_t;
+        using storage_t = typename simd_base<Crtp>::storage_t;
+        using binary_op_t = typename simd_base<Crtp>::binary_op_t;
+        using simd_base<Crtp>::width;
+        using simd_base<Crtp>::self;
+
+        SIMDEE_TRIVIAL_TYPE(dual_base);
+
+        SIMDEE_BASE_CTOR(dual_base, vector_t, mm = r);
+        SIMDEE_BASE_CTOR(dual_base, scalar_t, mm.first = r; mm.second = r);
+        SIMDEE_BASE_CTOR_TPL(dual_base, expr::aligned<T>, aligned_load(r.ptr));
+        SIMDEE_BASE_CTOR_TPL(dual_base, expr::unaligned<T>, unaligned_load(r.ptr));
+        SIMDEE_BASE_CTOR_TPL(dual_base, expr::init<T>, mm.first = r; mm.second = r);
+        SIMDEE_BASE_CTOR(dual_base, storage_t, aligned_load(r.data()));
+
+        SIMDEE_INL void aligned_load(const scalar_t* r) {
+            first.aligned_load(r);
+            second.aligned_load(r + T::width);
+        }
+
+        SIMDEE_INL void aligned_store(scalar_t* r) const {
+            first.aligned_store(r);
+            second.aligned_store(r + T::width);
+        }
+
+        SIMDEE_INL void unaligned_load(const scalar_t* r) {
+            first.unaligned_load(r);
+            second.unaligned_load(r + T::width);
+        }
+
+        SIMDEE_INL void unaligned_store(scalar_t* r) const {
+            first.unaligned_store(r);
+            second.unaligned_store(r + T::width);
+        }
+
+        SIMDEE_INL void interleaved_load(const scalar_t* r, std::size_t step) {
+            first.interleaved_load(r);
+            second.interleaved_load(r + (T::width * step));
+        }
+
+        SIMDEE_INL void interleaved_store(scalar_t* r, std::size_t step) const {
+            first.interleaved_store(r);
+            second.interleaved_store(r + (T::width * step));
+        }
+
+        // missing: reduce
+    };
+
     template <typename T>
-    struct dual_base : simd_base<dual<T>> {
+    struct dual<T, typename std::enable_if<std::is_same<T, typename T::vec_b>::value>::type> : dual_base<dual<T>> {
 
     };
 
     template <typename T>
-    struct dual<T, typename std::enable_if<std::is_same<T, typename T::vec_b>::value>::type> {
+    struct dual<T, typename std::enable_if<std::is_same<T, typename T::vec_f>::value>::type> : dual_base<dual<T>> {
 
     };
 
     template <typename T>
-    struct dual<T, typename std::enable_if<std::is_same<T, typename T::vec_f>::value>::type> {
+    struct dual<T, typename std::enable_if<std::is_same<T, typename T::vec_u>::value>::type> : dual_base<dual<T>> {
 
     };
 
     template <typename T>
-    struct dual<T, typename std::enable_if<std::is_same<T, typename T::vec_u>::value>::type> {
-
-    };
-
-    template <typename T>
-    struct dual<T, typename std::enable_if<std::is_same<T, typename T::vec_s>::value>::type> {
+    struct dual<T, typename std::enable_if<std::is_same<T, typename T::vec_s>::value>::type> : dual_base<dual<T>> {
 
     };
 }
